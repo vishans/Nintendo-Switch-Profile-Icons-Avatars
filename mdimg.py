2da3cc6d1627667108effaf5ab5dd2d4c4738f7c
Vishan Summinga Sonagadu <94801273+vishans@users.noreply.github.com>
2022-06-15 17:49:28 +0400

    16 bytes token file name only

--- mdimg.py
+++ mdimg.py
@@ -1,4 +1,5 @@
 import os
+p = '\\'.join((__file__.split('\\')[:-1]) + ['avatars\\'])
 
-for avatar in os.listdir('avatars'):
+for avatar in sorted(os.listdir('avatars'),key=lambda x: os.path.getmtime(p+x)):
     print(f'<img src="avatars/{avatar}">')
